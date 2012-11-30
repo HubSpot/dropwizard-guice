d2e672cff2944c4370683dd650d2009239e37cd4
Elias Torres <elias@torrez.us>
2012-11-30 08:39:13 -0500

    Separated AutoConfig from GuiceBundle and made it a fluid API

---
+++ src/main/java/com/hubspot/dropwizard/guice/AutoConfig.java
@@ -0,0 +1,111 @@
+package com.hubspot.dropwizard.guice;
+
+import java.util.Set;
+
+import javax.ws.rs.Path;
+import javax.ws.rs.ext.Provider;
+
+import org.reflections.Reflections;
+import org.reflections.scanners.SubTypesScanner;
+import org.reflections.scanners.TypeAnnotationsScanner;
+import org.reflections.util.ClasspathHelper;
+import org.reflections.util.ConfigurationBuilder;
+import org.reflections.util.FilterBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.common.base.Preconditions;
+import com.google.inject.Injector;
+import com.sun.jersey.spi.inject.InjectableProvider;
+import com.yammer.dropwizard.config.Environment;
+import com.yammer.dropwizard.lifecycle.Managed;
+import com.yammer.dropwizard.tasks.Task;
+import com.yammer.metrics.core.HealthCheck;
+
+public class AutoConfig {
+
+	final Logger logger = LoggerFactory.getLogger(AutoConfig.class);
+
+	private Reflections reflections;
+
+	public AutoConfig(String... basePackages) {
+		Preconditions.checkArgument(basePackages.length > 0);
+		
+		ConfigurationBuilder cfgBldr = new ConfigurationBuilder();
+		FilterBuilder filterBuilder = new FilterBuilder();
+		for (String basePkg : basePackages) {
+			cfgBldr.addUrls(ClasspathHelper.forPackage(basePkg));
+			filterBuilder.include(FilterBuilder.prefix(basePkg));
+		}
+
+		cfgBldr.filterInputsBy(filterBuilder).setScanners(
+				new SubTypesScanner(), new TypeAnnotationsScanner());
+		this.reflections = new Reflections(cfgBldr);
+	}
+
+	public void run(Environment environment, Injector injector) {
+		addHealthChecks(environment, injector);
+		addProviders(environment, injector);
+		addInjectableProviders(environment, injector);
+		addResources(environment, injector);
+		addTasks(environment, injector);
+		addManaged(environment, injector);
+	}
+
+	private void addManaged(Environment environment, Injector injector) {
+		Set<Class<? extends Managed>> managedClasses = reflections
+				.getSubTypesOf(Managed.class);
+		for (Class<? extends Managed> managed : managedClasses) {
+			environment.manage(injector.getInstance(managed));
+			logger.info("Added managed: " + managed);
+		}
+	}
+
+	private void addTasks(Environment environment, Injector injector) {
+		Set<Class<? extends Task>> taskClasses = reflections
+				.getSubTypesOf(Task.class);
+		for (Class<? extends Task> task : taskClasses) {
+			environment.addTask(injector.getInstance(task));
+			logger.info("Added task: " + task);
+		}
+	}
+
+	private void addHealthChecks(Environment environment, Injector injector) {
+		Set<Class<? extends HealthCheck>> healthCheckClasses = reflections
+				.getSubTypesOf(HealthCheck.class);
+		for (Class<? extends HealthCheck> healthCheck : healthCheckClasses) {
+			environment.addHealthCheck(injector.getInstance(healthCheck));
+			logger.info("Added healthCheck: " + healthCheck);
+		}
+	}
+
+	@SuppressWarnings("rawtypes")
+	private void addInjectableProviders(Environment environment,
+			Injector injector) {
+		Set<Class<? extends InjectableProvider>> injectableProviders = reflections
+				.getSubTypesOf(InjectableProvider.class);
+		for (Class<? extends InjectableProvider> injectableProvider : injectableProviders) {
+			environment.addProvider(injectableProvider);
+			logger.info("Added injectableProvider: " + injectableProvider);
+		}
+	}
+
+	private void addProviders(Environment environment, Injector injector) {
+		Set<Class<?>> providerClasses = reflections
+				.getTypesAnnotatedWith(Provider.class);
+		for (Class<?> provider : providerClasses) {
+			environment.addProvider(provider);
+			logger.info("Added provider class: " + provider);
+		}
+	}
+
+	private void addResources(Environment environment, Injector injector) {
+		Set<Class<?>> resourceClasses = reflections
+				.getTypesAnnotatedWith(Path.class);
+		for (Class<?> resource : resourceClasses) {
+			environment.addResource(resource);
+			logger.info("Added resource class: " + resource);
+		}
+	}
+
+}
--- src/main/java/com/hubspot/dropwizard/guice/GuiceBundle.java
+++ src/main/java/com/hubspot/dropwizard/guice/GuiceBundle.java
@@ -1,22 +1,15 @@
 package com.hubspot.dropwizard.guice;
 
 import java.util.Collection;
+import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
-import javax.ws.rs.Path;
-import javax.ws.rs.ext.Provider;
-
-import org.reflections.Reflections;
-import org.reflections.scanners.SubTypesScanner;
-import org.reflections.scanners.TypeAnnotationsScanner;
-import org.reflections.util.ClasspathHelper;
-import org.reflections.util.ConfigurationBuilder;
-import org.reflections.util.FilterBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.google.common.base.Preconditions;
 import com.google.common.collect.Lists;
+import com.google.inject.AbstractModule;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 import com.google.inject.Module;
@@ -25,46 +18,36 @@ import com.google.inject.util.Modules;
 import com.sun.jersey.api.core.ResourceConfig;
 import com.sun.jersey.guice.JerseyServletModule;
 import com.sun.jersey.spi.container.servlet.WebConfig;
-import com.sun.jersey.spi.inject.InjectableProvider;
 import com.yammer.dropwizard.ConfiguredBundle;
 import com.yammer.dropwizard.config.Bootstrap;
 import com.yammer.dropwizard.config.Configuration;
 import com.yammer.dropwizard.config.Environment;
-import com.yammer.dropwizard.lifecycle.Managed;
-import com.yammer.dropwizard.tasks.Task;
-import com.yammer.metrics.core.HealthCheck;
 
-public class GuiceBundle<T extends Configuration> implements
-		ConfiguredBundle<T> {
+public class GuiceBundle implements ConfiguredBundle<Configuration> {
 
 	final Logger logger = LoggerFactory.getLogger(GuiceBundle.class);
 
-	private Reflections reflections;
-
-	public GuiceBundle(String... basePackages) {
-		ConfigurationBuilder cfgBldr = new ConfigurationBuilder();
-		FilterBuilder filterBuilder = new FilterBuilder();
-		for (String basePkg : basePackages) {
-			cfgBldr.addUrls(ClasspathHelper.forPackage(basePkg));
-			filterBuilder.include(FilterBuilder.prefix(basePkg));
-		}
-
-		cfgBldr.filterInputsBy(filterBuilder).setScanners(
-				new SubTypesScanner(), new TypeAnnotationsScanner());
-		this.reflections = new Reflections(cfgBldr);
-	}
+	private AutoConfig autoConfig = null;
+	private List<Module> modules = Lists.newArrayList();
 
 	@Override
 	public void initialize(Bootstrap<?> bootstrap) {
 
 	}
 
-	protected Collection<? extends Module> configureModules(T configuration) {
-		return Lists.newArrayList();
+	public GuiceBundle addModule(Module module) {
+		Preconditions.checkNotNull(module);
+		modules.add(module);
+		return this;
+	}
+	
+	public GuiceBundle enableAutoConfig(String... basePackages) {
+		autoConfig = new AutoConfig(basePackages);
+		return this;
 	}
 
 	@Override
-	public void run(T configuration, final Environment environment) {
+	public void run(final Configuration configuration, final Environment environment) {
 		@SuppressWarnings("serial")
 		GuiceContainer container = new GuiceContainer() {
 			protected ResourceConfig getDefaultResourceConfig(
@@ -80,75 +63,17 @@ public class GuiceBundle<T extends Configuration> implements
 		Collection<Module> modules = Lists.newArrayList();
 		modules.add(Modules.override(new JerseyServletModule()).with(
 				new JerseyContainerModule(container)));
-		modules.addAll(configureModules(configuration));
+		modules.add(new AbstractModule() {
+
+			@Override
+			protected void configure() {
+				binder().bind(Configuration.class).toInstance(configuration);
+			}
+		});
+		modules.addAll(modules);
 		Injector injector = Guice.createInjector(modules);
-		if (reflections != null) {
-			autoConfigure(environment, injector);
-		}
-	}
-
-	private void autoConfigure(Environment environment, Injector injector) {
-		addHealthChecks(environment, injector);
-		addProviders(environment, injector);
-		addInjectableProviders(environment, injector);
-		addResources(environment, injector);
-		addTasks(environment, injector);
-		addManaged(environment, injector);
-	}
-
-	private void addManaged(Environment environment, Injector injector) {
-		Set<Class<? extends Managed>> managedClasses = reflections
-				.getSubTypesOf(Managed.class);
-		for (Class<? extends Managed> managed : managedClasses) {
-			environment.manage(injector.getInstance(managed));
-			logger.info("Added managed: " + managed);
-		}
-	}
-
-	private void addTasks(Environment environment, Injector injector) {
-		Set<Class<? extends Task>> taskClasses = reflections
-				.getSubTypesOf(Task.class);
-		for (Class<? extends Task> task : taskClasses) {
-			environment.addTask(injector.getInstance(task));
-			logger.info("Added task: " + task);
-		}
-	}
-
-	private void addHealthChecks(Environment environment, Injector injector) {
-		Set<Class<? extends HealthCheck>> healthCheckClasses = reflections
-				.getSubTypesOf(HealthCheck.class);
-		for (Class<? extends HealthCheck> healthCheck : healthCheckClasses) {
-			environment.addHealthCheck(injector.getInstance(healthCheck));
-			logger.info("Added healthCheck: " + healthCheck);
-		}
-	}
-
-	@SuppressWarnings("rawtypes")
-	private void addInjectableProviders(Environment environment,
-			Injector injector) {
-		Set<Class<? extends InjectableProvider>> injectableProviders = reflections
-				.getSubTypesOf(InjectableProvider.class);
-		for (Class<? extends InjectableProvider> injectableProvider : injectableProviders) {
-			environment.addProvider(injectableProvider);
-			logger.info("Added injectableProvider: " + injectableProvider);
-		}
-	}
-
-	private void addProviders(Environment environment, Injector injector) {
-		Set<Class<?>> providerClasses = reflections
-				.getTypesAnnotatedWith(Provider.class);
-		for (Class<?> provider : providerClasses) {
-			environment.addProvider(provider);
-			logger.info("Added provider class: " + provider);
-		}
-	}
-
-	private void addResources(Environment environment, Injector injector) {
-		Set<Class<?>> resourceClasses = reflections
-				.getTypesAnnotatedWith(Path.class);
-		for (Class<?> resource : resourceClasses) {
-			environment.addResource(resource);
-			logger.info("Added resource class: " + resource);
+		if (autoConfig != null) {
+			autoConfig.run(environment, injector);
 		}
 	}
 
