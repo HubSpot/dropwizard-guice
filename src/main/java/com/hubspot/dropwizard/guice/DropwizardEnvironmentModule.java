613f03921b2c65cd48250196f1b0c32b3e44607b
Mike Axiak <maxiak@hubspot.com>
2013-01-07 12:05:24 -0500

    Add the ability to specify custom configuration class during building of the bundle

--- src/main/java/com/hubspot/dropwizard/guice/DropwizardEnvironmentModule.java
+++ src/main/java/com/hubspot/dropwizard/guice/DropwizardEnvironmentModule.java
@@ -1,19 +1,31 @@
 package com.hubspot.dropwizard.guice;
 
+import com.google.common.base.Optional;
 import com.google.inject.AbstractModule;
+import com.google.inject.Provider;
 import com.google.inject.Provides;
 import com.yammer.dropwizard.config.Configuration;
 import com.yammer.dropwizard.config.Environment;
 
-public class DropwizardEnvironmentModule extends AbstractModule {
-	private Configuration configuration;
+public class DropwizardEnvironmentModule<T extends Configuration> extends AbstractModule {
+	private T configuration;
 	private Environment environment;
+	private Class<? super T> configurationClass;
+
+	public DropwizardEnvironmentModule(Class<T> configurationClass) {
+		this.configurationClass = configurationClass;
+	}
 
 	@Override
 	protected void configure() {
+		Provider<T> provider = new CustomConfigurationProvider();
+		bind(configurationClass).toProvider(provider);
+		if (configurationClass != Configuration.class) {
+			bind(Configuration.class).toProvider(provider);
+		}
 	}
 
-	public void setEnvironmentData(Configuration configuration, Environment environment) {
+	public void setEnvironmentData(T configuration, Environment environment) {
 		this.configuration = configuration;
 		this.environment = environment;
 	}
@@ -23,8 +35,10 @@ public class DropwizardEnvironmentModule extends AbstractModule {
 		return environment;
 	}
 
-	@Provides
-	public Configuration providesConfiguration() {
-		return configuration;
+	private class CustomConfigurationProvider implements Provider<T> {
+		@Override
+		public T get() {
+			return configuration;
+		}
 	}
 }
