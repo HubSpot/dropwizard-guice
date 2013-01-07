f268c54d0f0b4bd9cc3367f997e89d37ce876e83
Mike Axiak <maxiak@hubspot.com>
2013-01-07 12:08:47 -0500

    Throw exceptions if accessing configuration/environment during bootstrap phase.

--- src/main/java/com/hubspot/dropwizard/guice/DropwizardEnvironmentModule.java
+++ src/main/java/com/hubspot/dropwizard/guice/DropwizardEnvironmentModule.java
@@ -2,10 +2,14 @@ package com.hubspot.dropwizard.guice;
 
 import com.google.common.base.Optional;
 import com.google.inject.AbstractModule;
+import com.google.inject.CreationException;
 import com.google.inject.Provider;
 import com.google.inject.Provides;
+import com.google.inject.spi.Message;
 import com.yammer.dropwizard.config.Configuration;
 import com.yammer.dropwizard.config.Environment;
+
+import java.util.Arrays;
 
 public class DropwizardEnvironmentModule<T extends Configuration> extends AbstractModule {
 	private T configuration;
@@ -32,12 +36,18 @@ public class DropwizardEnvironmentModule<T extends Configuration> extends Abstra
 
 	@Provides
 	public Environment providesEnvironment() {
+		if (environment == null) {
+			throw new CreationException(Arrays.asList(new Message("The dropwizard environment has not yet been set. This is likely caused by trying to access the dropwizard environment during the bootstrap phase.")));
+		}
 		return environment;
 	}
 
 	private class CustomConfigurationProvider implements Provider<T> {
 		@Override
 		public T get() {
+			if (configuration == null) {
+				throw new CreationException(Arrays.asList(new Message("The dropwizard configuration has not yet been set. This is likely caused by trying to access the dropwizard environment during the bootstrap phase.")));
+			}
 			return configuration;
 		}
 	}
