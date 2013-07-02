16c8818b3e2d1de0de03c3ac4e1e65730338040b
David Morgantini <dmorgantini@gmail.com>
2013-07-02 10:32:16 +0100

    David - upgraded to support dropwizard 0.7.0-SNAPSHOT

--- src/main/java/com/hubspot/dropwizard/guice/DropwizardEnvironmentModule.java
+++ src/main/java/com/hubspot/dropwizard/guice/DropwizardEnvironmentModule.java
@@ -1,11 +1,11 @@
 package com.hubspot.dropwizard.guice;
 
+import com.codahale.dropwizard.Configuration;
+import com.codahale.dropwizard.setup.Environment;
 import com.google.inject.AbstractModule;
 import com.google.inject.Provider;
 import com.google.inject.Provides;
 import com.google.inject.ProvisionException;
-import com.yammer.dropwizard.config.Configuration;
-import com.yammer.dropwizard.config.Environment;
 
 public class DropwizardEnvironmentModule<T extends Configuration> extends AbstractModule {
 	private static final String ILLEGAL_DROPWIZARD_MODULE_STATE = "The dropwizard environment has not yet been set. This is likely caused by trying to access the dropwizard environment during the bootstrap phase.";
--- src/main/java/com/hubspot/dropwizard/guice/GuiceBundle.java
+++ src/main/java/com/hubspot/dropwizard/guice/GuiceBundle.java
@@ -1,111 +1,118 @@
 package com.hubspot.dropwizard.guice;
 
-import java.util.List;
-
+import com.codahale.dropwizard.Configuration;
+import com.codahale.dropwizard.ConfiguredBundle;
+import com.codahale.dropwizard.setup.Bootstrap;
+import com.codahale.dropwizard.setup.Environment;
+import com.google.common.base.Function;
 import com.google.common.base.Optional;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import com.google.common.base.Preconditions;
 import com.google.common.collect.Lists;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 import com.google.inject.Module;
 import com.google.inject.servlet.GuiceFilter;
-import com.google.inject.util.Modules;
-import com.sun.jersey.guice.JerseyServletModule;
-import com.yammer.dropwizard.ConfiguredBundle;
-import com.yammer.dropwizard.config.Bootstrap;
-import com.yammer.dropwizard.config.Configuration;
-import com.yammer.dropwizard.config.Environment;
+import com.sun.jersey.api.core.ResourceConfig;
+import com.sun.jersey.spi.container.servlet.ServletContainer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.annotation.Nullable;
+import java.util.List;
 
 public class GuiceBundle<T extends Configuration> implements ConfiguredBundle<T> {
 
-	final Logger logger = LoggerFactory.getLogger(GuiceBundle.class);
+    final Logger logger = LoggerFactory.getLogger(GuiceBundle.class);
 
-	private final AutoConfig autoConfig;
-	private final List<Module> modules;
-	private Injector injector;
-	private DropwizardEnvironmentModule dropwizardEnvironmentModule;
-	private Optional<Class<T>> configurationClass;
-	private GuiceContainer container;
-	
-	public static class Builder<T extends Configuration> {
-		private AutoConfig autoConfig;
-		private List<Module> modules = Lists.newArrayList();
-		private Optional<Class<T>> configurationClass = Optional.<Class<T>>absent();
-		
-		public Builder<T> addModule(Module module) {
-			Preconditions.checkNotNull(module);
-			modules.add(module);
-			return this;
-		}
+    private final AutoConfig autoConfig;
+    private final List<Module> modules;
+    private Injector injector;
+    private DropwizardEnvironmentModule dropwizardEnvironmentModule;
+    private Optional<Class<T>> configurationClass;
+    private GuiceContainer container;
 
-		public Builder<T> setConfigClass(Class<T> clazz) {
-			configurationClass = Optional.of(clazz);
-			return this;
-		}
+    public static class Builder<T extends Configuration> {
+        private AutoConfig autoConfig;
+        private List<Module> modules = Lists.newArrayList();
+        private Optional<Class<T>> configurationClass = Optional.<Class<T>>absent();
 
-		public Builder<T> enableAutoConfig(String... basePackages) {
-			Preconditions.checkNotNull(basePackages.length > 0);
-			Preconditions.checkArgument(autoConfig == null, "autoConfig already enabled!");
-			autoConfig = new AutoConfig(basePackages);
-			return this;
-		}
-		
-		public GuiceBundle<T> build() {
-			return new GuiceBundle<T>(autoConfig, modules, configurationClass);
-		}
+        public Builder<T> addModule(Module module) {
+            Preconditions.checkNotNull(module);
+            modules.add(module);
+            return this;
+        }
 
-	}
-	
-	public static <T extends Configuration> Builder<T> newBuilder() {
-		return new Builder<T>();
-	}
+        public Builder<T> setConfigClass(Class<T> clazz) {
+            configurationClass = Optional.of(clazz);
+            return this;
+        }
 
-	private GuiceBundle(AutoConfig autoConfig, List<Module> modules, Optional<Class<T>> configurationClass) {
-		Preconditions.checkNotNull(modules);
-		Preconditions.checkArgument(!modules.isEmpty());
-		this.modules = modules;
-		this.autoConfig = autoConfig;
-		this.configurationClass = configurationClass;
-	}
-	
-	@Override
-	public void initialize(Bootstrap<?> bootstrap) {
-		container = new GuiceContainer();
-		JerseyContainerModule jerseyContainerModule = new JerseyContainerModule(container);
-		if (configurationClass.isPresent()) {
-			dropwizardEnvironmentModule = new DropwizardEnvironmentModule<T>(configurationClass.get());
-		} else {
-			dropwizardEnvironmentModule = new DropwizardEnvironmentModule<Configuration>(Configuration.class);
-		}
-		modules.add(jerseyContainerModule);
-		modules.add(dropwizardEnvironmentModule);
-		injector = Guice.createInjector(modules);
-		if (autoConfig != null) {
-			autoConfig.initialize(bootstrap, injector);
-		}
-	}
+        public Builder<T> enableAutoConfig(String... basePackages) {
+            Preconditions.checkNotNull(basePackages.length > 0);
+            Preconditions.checkArgument(autoConfig == null, "autoConfig already enabled!");
+            autoConfig = new AutoConfig(basePackages);
+            return this;
+        }
 
-	@Override
-	public void run(final T configuration, final Environment environment) {
-		container.setResourceConfig(environment.getJerseyResourceConfig());
-		environment.setJerseyServletContainer(container);
-		environment.addFilter(GuiceFilter.class, configuration.getHttpConfiguration().getRootPath());
-		setEnvironment(configuration, environment);
+        public GuiceBundle<T> build() {
+            return new GuiceBundle<T>(autoConfig, modules, configurationClass);
+        }
 
-		if (autoConfig != null) {
-			autoConfig.run(environment, injector);
-		}
-	}
+    }
 
-	@SuppressWarnings("unchecked")
-	private void setEnvironment(final T configuration, final Environment environment) {
-		dropwizardEnvironmentModule.setEnvironmentData(configuration, environment);
-	}
+    public static <T extends Configuration> Builder<T> newBuilder() {
+        return new Builder<T>();
+    }
 
-	public Injector getInjector() {
-		return injector;
-	}
+    private GuiceBundle(AutoConfig autoConfig, List<Module> modules, Optional<Class<T>> configurationClass) {
+        Preconditions.checkNotNull(modules);
+        Preconditions.checkArgument(!modules.isEmpty());
+        this.modules = modules;
+        this.autoConfig = autoConfig;
+        this.configurationClass = configurationClass;
+    }
+
+    @Override
+    public void initialize(Bootstrap<?> bootstrap) {
+        container = new GuiceContainer();
+        JerseyContainerModule jerseyContainerModule = new JerseyContainerModule(container);
+        if (configurationClass.isPresent()) {
+            dropwizardEnvironmentModule = new DropwizardEnvironmentModule<T>(configurationClass.get());
+        } else {
+            dropwizardEnvironmentModule = new DropwizardEnvironmentModule<Configuration>(Configuration.class);
+        }
+        modules.add(jerseyContainerModule);
+        modules.add(dropwizardEnvironmentModule);
+        injector = Guice.createInjector(modules);
+        if (autoConfig != null) {
+            autoConfig.initialize(bootstrap, injector);
+        }
+    }
+
+    @Override
+    public void run(final T configuration, final Environment environment) {
+        container.setResourceConfig(environment.jersey().getResourceConfig());
+        environment.jersey().replace(new Function<ResourceConfig, ServletContainer>() {
+            @Nullable
+            @Override
+            public ServletContainer apply(ResourceConfig resourceConfig) {
+                return container;
+            }
+        });
+        environment.servlets().addFilter("Guice Filter", GuiceFilter.class);
+        setEnvironment(configuration, environment);
+
+        if (autoConfig != null) {
+            autoConfig.run(environment, injector);
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    private void setEnvironment(final T configuration, final Environment environment) {
+        dropwizardEnvironmentModule.setEnvironmentData(configuration, environment);
+    }
+
+    public Injector getInjector() {
+        return injector;
+    }
 }
