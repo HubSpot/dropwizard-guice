5d2566c8207729a0ea50eb8220b537a1d3f82eb0
Elias Torres <elias@torrez.us>
2014-10-08 11:29:09 -0400

    Changes to work with latest Guice4.0-beta5

--- src/main/java/com/hubspot/dropwizard/guice/JerseyContainerModule.java
+++ src/main/java/com/hubspot/dropwizard/guice/JerseyContainerModule.java
@@ -1,22 +1,107 @@
 package com.hubspot.dropwizard.guice;
 
-import com.sun.jersey.guice.JerseyServletModule;
+import com.google.inject.Provides;
+import com.google.inject.servlet.RequestScoped;
+import com.google.inject.servlet.ServletModule;
+import com.sun.jersey.api.core.*;
+import com.sun.jersey.core.util.FeaturesAndProperties;
+import com.sun.jersey.spi.MessageBodyWorkers;
+import com.sun.jersey.spi.container.ExceptionMapperContext;
 import com.sun.jersey.spi.container.WebApplication;
 
-public class JerseyContainerModule extends JerseyServletModule {
-    private final GuiceContainer container;
+import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.Request;
+import javax.ws.rs.core.SecurityContext;
+import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.ext.Providers;
 
-    public JerseyContainerModule(final GuiceContainer container) {
-        this.container = container;
-    }
+public class JerseyContainerModule extends ServletModule {
+  private final GuiceContainer container;
 
-    @Override
-    protected void configureServlets() {
-        bind(GuiceContainer.class).toInstance(container);
-    }
+  public JerseyContainerModule(final GuiceContainer container) {
+    this.container = container;
+  }
 
-    @Override
-    public WebApplication webApp(com.sun.jersey.guice.spi.container.servlet.GuiceContainer guiceContainer) {
-        return container.getWebApplication();
-    }
+  @Override
+  protected void configureServlets() {
+    bind(GuiceContainer.class).toInstance(container);
+  }
+
+  @Provides
+  public WebApplication webApp(GuiceContainer guiceContainer) {
+    return container.getWebApplication();
+  }
+
+  @Provides
+  public Providers providers(WebApplication webApplication) {
+    return webApplication.getProviders();
+  }
+
+  @Provides
+  public FeaturesAndProperties featuresAndProperties(WebApplication webApplication) {
+    return webApplication.getFeaturesAndProperties();
+  }
+
+  @Provides
+  public MessageBodyWorkers messageBodyWorkers(WebApplication webApplication) {
+    return webApplication.getMessageBodyWorkers();
+  }
+
+  @Provides
+  public ExceptionMapperContext exceptionMapperContext(WebApplication webApplication) {
+    return webApplication.getExceptionMapperContext();
+  }
+
+  @Provides
+  public ResourceContext resourceContext(WebApplication webApplication) {
+    return webApplication.getResourceContext();
+  }
+
+  @RequestScoped
+  @Provides
+  public HttpContext httpContext(WebApplication webApplication) {
+    return webApplication.getThreadLocalHttpContext();
+  }
+
+  @Provides
+  @RequestScoped
+  public UriInfo uriInfo(WebApplication wa) {
+    return wa.getThreadLocalHttpContext().getUriInfo();
+  }
+
+  @Provides
+  @RequestScoped
+  public ExtendedUriInfo extendedUriInfo(WebApplication wa) {
+    return wa.getThreadLocalHttpContext().getUriInfo();
+  }
+
+  @RequestScoped
+  @Provides
+  public HttpRequestContext requestContext(WebApplication wa) {
+    return wa.getThreadLocalHttpContext().getRequest();
+  }
+
+  @RequestScoped
+  @Provides
+  public HttpHeaders httpHeaders(WebApplication wa) {
+    return wa.getThreadLocalHttpContext().getRequest();
+  }
+
+  @RequestScoped
+  @Provides
+  public Request request(WebApplication wa) {
+    return wa.getThreadLocalHttpContext().getRequest();
+  }
+
+  @RequestScoped
+  @Provides
+  public SecurityContext securityContext(WebApplication wa) {
+    return wa.getThreadLocalHttpContext().getRequest();
+  }
+
+  @RequestScoped
+  @Provides
+  public HttpResponseContext responseContext(WebApplication wa) {
+    return wa.getThreadLocalHttpContext().getResponse();
+  }
 }
